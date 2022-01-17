682cf499dcd443cb0c94dd8b535688b3989ba0b1
crascot <y-sitdikov@mail.ru>
2022-01-17 15:15:39 +0600

    ref profile

--- src/feed/profile/profile.jsx
+++ src/feed/profile/profile.jsx
@@ -4,7 +4,6 @@ import User from './user';
 import Info from './info';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@mui/material/Paper';
-import { DB, reg } from '../../services/services';
 
 const useStyles = makeStyles({
     profile: {
@@ -21,8 +20,6 @@ const Profile = ({ feedUser }) => {
     const classes = useStyles();
 
     const [name, setName] = useState(feedUser.name)
-
-    const filtered = DB.users.filter(user => user.name !== feedUser.name ? false : user)
 
     return (
         <Container className={classes.profile}>
